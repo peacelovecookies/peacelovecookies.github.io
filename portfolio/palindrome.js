b25bf095b9eca959bfd81835d605020d0bffcf70
peacelovecookies <kimpav95@mail.ru>
2018-04-12 21:37:54 +0500

    Removed portfolio from repository

--- portfolio/palindrome.js
+++
@@ -1,6 +0,0 @@
-const palindrome = str => {
-  const filtered = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
-  const reversed = filtered.split('').reverse().join('');
-  return reversed === filtered;
-  };
-export default palindrome;
