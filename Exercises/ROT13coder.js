0aa6b023835fc182c77d39f638250555ddcc4f22
peacelovecookies <kimpav95@mail.ru>
2018-04-12 21:34:59 +0500

    rename portfolio to Exercises

---
+++ Exercises/ROT13coder.js
@@ -0,0 +1,16 @@
+const rot13 = (str) => {
+  const table = {
+    A: 'N', B: 'O', C: 'P', D: 'Q',
+    E: 'R', F: 'S', G: 'T', H: 'U',
+    I: 'V', J: 'W', K: 'X', L: 'Y', 
+    M: 'Z', 
+    N: 'A', O: 'B', P: 'C', Q: 'D',
+    R: 'E', S: 'F', T: 'G', U: 'H',
+    V: 'I', W: 'J', X: 'K', Y: 'L', 
+    Z: 'M',
+  };
+  const decoded = str.split('').map(elem => /\w/.test(elem) ? table[elem] : elem).join('');
+  return decoded;
+};
+
+export default rot13;
---
+++ Exercises/palindrome.js
@@ -0,0 +1,6 @@
+const palindrome = (str) => {
+  const filtered = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+  const reversed = filtered.split('').reverse().join('');
+  return reversed === filtered;
+  };
+export default palindrome;
